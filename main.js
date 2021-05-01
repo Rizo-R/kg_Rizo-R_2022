d011c8c3326b6cb8234cd180e48a15bd368d5a69
Rizo-R <rizorim@gmail.com>
2021-05-01 18:52:05 -0400

    Now functional with negative numbers

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
+/* Precondition: arr is an Array of integers. Negative integers allowed. */
 function convert_phonetic(arr) {
-    // Precondition: all numbers are non-negative.
     var res = [];
     for (let i = 0; i < arr.length; i++) {
         res.push(phon_equiv(arr[i]));
@@ -7,6 +7,9 @@ function convert_phonetic(arr) {
     return res;
 }
 
+/* Converts a number to its phonetic equivalent.
+ * Precondition: num is an integer. 
+ */
 function phon_equiv(num) {
     // Create a dictionary of phonetic equivalents
     var dict = {
@@ -22,12 +25,18 @@ function phon_equiv(num) {
         9: "Nine",
     }
     var res = "";
-    // Assuming num >= 0
+    // Check if num is negative before taking the absolute value
+    var is_negative = num < 0;
+    num = Math.abs(num);
+    // Keep taking remainder until num < 10
     while (num >= 10) {
         res = dict[num % 10] + res;
         num = Math.floor(num / 10);
     }
+    // Append the last digit
     res = dict[num] + res;
+    // Append the "Minus" if num was negative
+    if (is_negative) res = "Minus" + res;
     return res;
 }
 
